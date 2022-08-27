7e8fa26a51511e8b2b86a6e99714920c250b3f56
Eddiebee <edbeeotudor@gmail.com>
2022-08-27 12:34:23 +0100

    Build up emitter functionality

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Node event emitter!");
+const logEvents = require("./logEvents");
+
+const EventEmitter = require("events");
+
+// define emitter class
+class MyEmitter extends EventEmitter {}
+
+// create instance of MyEmiiter class
+const myEmitter = new MyEmitter();
+
+// add listener
+myEmitter.on("log", (message) => logEvents(message));
+
+// setTimeout added just to make the emission more observable... cool, right? lol
+setTimeout(() => {
+  // emit log event
+  myEmitter.emit("log", "Log event emitted");
+}, 3000);
